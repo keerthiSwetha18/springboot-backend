bd5962d395a6ee70bdf1f977a593be5710a09dc0
talasilar <talasilar@hotmail.com>
2025-08-21 20:11:53 -0400

    IDEN:3535 : Fixes

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 @RestController
-@RequestMapping("/api)")
+@RequestMapping("/api")
 public class EmployeeController {
 
     @Autowired
